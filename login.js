08bb3a33a7bf55d7902ce36b1752d1b0f520d98c
sabbir <sabbirhossain1177034@gmail.com>
2026-03-06 23:08:45 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-login').addEventListener('click', function(){
+    console.log('login button clicked')
+    // 1-get the input name
+    const inputNumber = document.getElementById('input-name');
+    const contactNumber = inputNumber.value;
+    console.log(contactNumber)
+    //2-get the pin input
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    console.log(pin)
+
+
+    //3-match pin & name
+    if(contactNumber == 'admin' && pin == 'admin123'){
+        console.log("login success");
+        alert('login success');
+        window.location.assign('/index.html')
+    }
+    else{
+    alert('login failed')
+    return;
+    }
+})
