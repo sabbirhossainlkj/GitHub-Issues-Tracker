7b4a536b262a1986b8d35c04053b06ca7367f500
sabbir <sabbirhossain1177034@gmail.com>
2026-03-08 17:02:38 +0600

    complete modal & button active

--- index.js
+++ index.js
@@ -45,42 +45,37 @@ activeBtn.classList.add('btn-primary','text-white');
 
 }
 
-// assignee: "jane_smith"
-// author: "john_doe"
-// createdAt: "2024-01-15T10:30:00Z"
-// description: "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior."
-// id: 1
-// labels: (2) ['bug', 'help wanted']
-// priority: "high"
-// status: "open"
-// title: "Fix navigation menu on mobile devices"
-// updatedAt: "2024-01-15T10:30:00Z
 const displayAll = (posts) => {
     //1 get the container and empty the container
  const allCard = document.getElementById('all-card');
+ document.getElementById('issue-count').innerText = posts.length + " Issues";
+
+
  allCard.innerHTML='';
 // console.log(posts)
 posts.forEach(post => {
     // console.log(post)
     //2 get element
+    
     const card = document.createElement('div');
+    card.onclick = () => showModal(post); 
     card.innerHTML=`<div class="card shadow-xl border-t-4 border-[#00A96E]  p-4 bg-indigo-200 space-y-2" >
     <div class="flex justify-between items-center">
         <img src="./assets/Open-Status.png" alt="">
         <p class="border rounded-[50px] px-3
          text-white bg-orange-300">${post.priority}</p>
     </div>
-    <h2 class="font-semibold text-xl">Fix navigation menu on mobile devices</h2>
+    <h2 class="font-semibold text-xl">${post.title}</h2>
     <p class="text-neutral-400 text-sm">
     ${post.title}</p>
     <div class="flex gap-3">
-        <p class="rounded-full px-3 border
+        <p class="rounded-full flex items-center  px-3 border
          bg-orange-100 text-[#EF4444] ">
-         <img src="./assets/Vector (2).png" alt="">${post.labels[0]}</p>
+         <img class="w-4" src="./assets/Vector (2).png" alt="">${post.labels[0]}</p>
         
         ${post.labels[1] ? `
-        <p class="rounded-full px-4 border bg-[#FFF8DB] text-[#D97706]">
-        <img src="./assets/Vector (3).png" alt=""> ${post.labels[1]}
+        <p class="rounded-full flex items-center px-2 border bg-[#FFF8DB] text-[#D97706]">
+        <img class="w-4" src="./assets/Vector (3).png" alt=""> ${post.labels[1]}
         </p>
         ` : ''}
     </div>
@@ -92,9 +87,46 @@ posts.forEach(post => {
     allCard.appendChild(card)
 })
 }
+function showModal(post){
+
+document.getElementById('modal-title').innerText = post.title;
+
+document.getElementById('modal-meta').innerText =
+`Opened by ${post.author} • ${post.createdAt}`;
+
+document.getElementById('modal-description').innerText = post.description;
+
+document.getElementById('modal-assignee').innerText = post.assignee;
+
+document.getElementById('modal-priority').innerText = post.priority.toUpperCase();
+
+document.getElementById('modal-label1').innerText = post.labels[0] || '';
+document.getElementById('modal-label2').innerText = post.labels[1] || '';
+
+document.getElementById('issue-modal').showModal();
+
+}
+
+function closeModal(){
+document.getElementById('issue-modal').close();
+}
 loaddata();
 
+document.getElementById('btn-search').addEventListener('click', searchIssue);
+function searchIssue(){
 
+const searchValue = document
+.getElementById('input-search')
+.value
+.toLowerCase();
+
+const filtered = allIssues.filter(issue =>
+issue.title.toLowerCase().includes(searchValue)
+);
+
+displayAll(filtered);
+
+}
 
 
 
